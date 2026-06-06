5440ab5dd9be60b8456417fc5ed34e674d6eb9fa
harshN26 <hnihalani26@gmail.com>
2026-06-06 17:36:42 -0400

    fix deployment issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
+    reactStrictMode: true,
     images: {
         unoptimized: true
-    },
-    trailingSlash: true,
-    basePath: process.env.NODE_ENV === "production"
-        ? "/harshn26.github.io"
-        : ""
+    }
 };
 
 export default nextConfig;
